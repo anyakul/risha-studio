df28dfd853c44be7ed91f7dfef37e77c5d40f7e4
anyakul <anyakulikova111@gmail.com>
2020-09-06 20:22:53 +0300

    Делаю горизонтальную десктопную версию сайта и вертикальные мобильную и планшетную

---
+++ build/js/scroll.js
@@ -0,0 +1,29 @@
+var main = document.querySelector('.main');
+var isDesktop=true;
+if (main.offsetWidth <= 1199) {
+  isDesktop=false;
+}
+
+function scrollHorizontally(e) {
+  if (isDesktop) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.querySelector('.main__wrapper').scrollLeft -= (delta*100); // Multiplied by 10
+        e.preventDefault();
+    }
+}
+      if (document.querySelector('.main__wrapper').addEventListener) {
+          // IE9, Chrome, Safari, Opera
+          document.querySelector('.main__wrapper').addEventListener("mousewheel", scrollHorizontally, false);
+          document.querySelector('.main__wrapper').addEventListener("ontouchmove", scrollHorizontally, false);
+          // Firefox
+          document.querySelector('.main__wrapper').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+      } else {
+          // IE 6/7/8
+          document.querySelector('.main__wrapper').attachEvent("onmousewheel", scrollHorizontally);
+      }
+
+
+
+console.log(main);
+console.log(scrollHorizontally(e));
